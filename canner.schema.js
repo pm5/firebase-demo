9de8ef136987940c505075b93de7f8bcdbdbb72c
JackHou <vjack070707@gmail.com>
2017-05-31 22:57:48 +0800

    rm

--- canner.schema.js
+++
@@ -1,6 +0,0 @@
-var title = CannerTypes.string().description('標題');
-var content = CannerTypes.string().description('內容').ui('editor');
-
-var posts = CannerTypes.array({title, content}).description('文章');
-
-module.exports = {posts};
