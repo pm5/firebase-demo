beda2e62936abc91d866f9dc658a5485e3f9ee87
howardchi <howardchi@fougood.com>
2017-11-11 14:19:42 +0800

    add about page

--- canner.schema.js
+++ canner.schema.js
@@ -1,9 +1,17 @@
 // declares type is string and description is Title
-var title = CannerTypes.string().description('Title');
+var title = CannerTypes
+              .string()
+              .description('Title');
+
 // declares the plugin type is string and the description is Content, display as a editor
-var content = CannerTypes.string().description('Content').ui('editor');
+var content = CannerTypes
+              .string()
+              .description('Content')
+              .ui('editor');
 
 // posts is an array, each post in it contains title and content
-var posts = CannerTypes.array({title, content}).description('Posts');
+var posts = CannerTypes
+              .array({title, content})
+              .description('Posts');
 
 module.exports = {posts};
